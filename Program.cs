24569116247726efbd951b9948c37e9c11c71962
salih <salih.safaa@switch.com.iq>
2026-08-11 14:53:34 +0300

    day2 task, creating a class for product and category and using them in program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("testing the repo");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+
+            Category category =new Category();
+            category.Id = 1;
+            category.Name = "Electronics";
+            category.Description = "Electronic devices";
+
+
+            Product product =new Product();
+            product.Id = 1;
+            product.Name = "Laptop";
+            product.Price = 1000.00m;
+
+
+            Console.WriteLine($"Category: {category.Name}, Description: {category.Description}");
+            Console.WriteLine($"Product: {product.Name}, Price: {product.Price}");
+
+            
+        }
+    }
+}
--- obj/Debug/net10.0/hello world.AssemblyInfo.cs
+++ obj/Debug/net10.0/hello world.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello world")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0fb12c8dcc16b9976db8d1e152b0673ac455ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b5145cf1af090b14f904a3c5e029b08a2631787")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello world")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello world")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
